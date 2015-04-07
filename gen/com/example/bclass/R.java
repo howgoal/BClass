aa2a57d4c9afc8172de6b760ed616386ed369fa7
LinSinSin <xxice520@hotmail.com>
2015-04-07 21:57:22 +0800

    TVoteActivity

--- gen/com/example/bclass/R.java
+++ gen/com/example/bclass/R.java
@@ -25,11 +25,17 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080004;
+        public static final int FrameLayout1=0x7f080005;
+        public static final int LinearLayout1=0x7f080006;
+        public static final int LinearLayout2=0x7f080008;
+        public static final int action_settings=0x7f08000a;
+        public static final int btnCreatVote=0x7f080004;
         public static final int btn_student=0x7f080001;
         public static final int btn_teacher=0x7f080000;
         public static final int container=0x7f080003;
         public static final int textView1=0x7f080002;
+        public static final int vote_day=0x7f080009;
+        public static final int vote_name=0x7f080007;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -38,6 +44,7 @@ public final class R {
         public static final int activity_student=0x7f030003;
         public static final int activity_teacher=0x7f030004;
         public static final int activity_tvote=0x7f030005;
+        public static final int vote_list_item=0x7f030006;
     }
     public static final class menu {
         public static final int main=0x7f070000;
