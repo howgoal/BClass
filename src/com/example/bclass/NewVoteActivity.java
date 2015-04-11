577557724184212bb88afa4bb4201281d3f374db
outxian <sakaur00@gmail.com>
2015-04-11 14:16:49 +0800

    add newvote

--- src/com/example/bclass/NewVoteActivity.java
+++ src/com/example/bclass/NewVoteActivity.java
@@ -5,22 +5,134 @@ import com.parse.ParseObject;
 import android.app.Activity;
 import android.app.ActionBar;
 import android.app.Fragment;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.View.OnClickListener;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 import android.os.Build;
 
 public class NewVoteActivity extends Activity {
-
+ 
+	private String[] choice = {"2", "3", "4", "5", "6", "7"};
+	private String[] time = {"3", "5", "10", "15", "30"};
+	private Context testContext;
+	private String title = "";
+	private String detail = "";
+	private int choice_OK = 2; 
+	private int time_OK = 3;
+	private EditText vote_title;
+	private EditText vote_detail;
+	private Spinner spinnerChoice;
+	private Spinner spinnerTime;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_newvote);
-		
+		init();
 	}
+	
+	public void init() {
+		vote_title = (EditText) findViewById(R.id.voteTitle);
+		vote_detail = (EditText) findViewById(R.id.voteDetail);
+		TextView vote_choice = (TextView) findViewById(R.id.voteChoice);
+		TextView vote_time = (TextView) findViewById(R.id.voteTime);
+		Button btn_OK = (Button) findViewById(R.id.voteOK);
+		Button btn_cancel = (Button) findViewById(R.id.voteCancel);
+		spinnerChoice = (Spinner) findViewById(R.id.spinnerChoice);
+		spinnerTime = (Spinner) findViewById(R.id.spinnerTime);
+		ArrayAdapter<String> adapterChoice = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, choice);
+		ArrayAdapter<String> adapterTime = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, time);
+		
+	    btn_OK.setOnClickListener(decision);
+	    btn_cancel.setOnClickListener(decision);
+		spinnerChoice.setAdapter(adapterChoice);
+		spinnerChoice.setOnItemSelectedListener(spinnerListener);
+		spinnerChoice.setVisibility(View.VISIBLE);
+		spinnerTime.setAdapter(adapterTime);
+		spinnerTime.setOnItemSelectedListener(spinnerListener);
+		spinnerTime.setVisibility(View.VISIBLE);
+		
+		testContext = this.getApplicationContext();
+	}
+	
+	private OnClickListener decision = new OnClickListener() {
+		
+		@Override
+		public void onClick(View v) {
+			// TODO Auto-generated method stub
+			switch (v.getId()) {
+			case R.id.voteOK:
+				title = vote_title.getText().toString();
+				detail = vote_detail.getText().toString();
+				ParseObject vote = new ParseObject("Vote");
+				vote.put("description", detail);
+				vote.put("name", title);
+				vote.put("choise", choice_OK);
+				vote.put("time", time_OK);
+				vote.saveInBackground();
+				
+				Toast.makeText(NewVoteActivity.this, "資料已創建完成", Toast.LENGTH_SHORT).show();
+				vote_title.setText("");
+				vote_detail.setText("");
+				spinnerChoice.setSelection(0);
+				spinnerTime.setSelection(0);
+				break;
+			case R.id.voteCancel:
+				Intent intent_tvote = new Intent();
+				intent_tvote.setClass(NewVoteActivity.this, TVoteActivity.class);
+				startActivity(intent_tvote); 
+				NewVoteActivity.this.finish(); 
+				break;
+			default:
+				break;
+			}
+		}
+	};
+	
+	private OnItemSelectedListener spinnerListener = new OnItemSelectedListener() {
+
+		@Override
+		public void onItemSelected(AdapterView<?> parent, View view,
+				int position, long id) {
+			// TODO Auto-generated method stub
+			switch (parent.getId()) {
+			case R.id.spinnerChoice:
+				Log.i("@@", choice[position]);
+				choice_OK = Integer.parseInt(choice[position]);
+				//Toast.makeText(testContext, "Choice: "+choice[position], Toast.LENGTH_SHORT).show();
+				break;
+			case R.id.spinnerTime:
+				Log.i("@@", time[position]);
+				time_OK = Integer.parseInt(time[position]);
+				//Toast.makeText(testContext, "Time: "+time[position], Toast.LENGTH_SHORT).show();
+				break;
+			default:
+				break;
+			}
+		}
+
+		@Override
+		public void onNothingSelected(AdapterView<?> parent) {
+			// TODO Auto-generated method stub
+			
+		}
+		
+	};
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
--- src/com/example/bclass/TeacherActivity.java
+++ src/com/example/bclass/TeacherActivity.java
@@ -27,8 +27,10 @@ public class TeacherActivity extends Activity {
 	public void init() {
 		Button btn_TBack = (Button) findViewById(R.id.btn_TBack);
 		Button btn_ToVote = (Button) findViewById(R.id.btn_TToVote);
+		Button btn_testButton = (Button) findViewById(R.id.btn_Test); // test
 		btn_TBack.setOnClickListener(modeChange);
 		btn_ToVote.setOnClickListener(modeChange);
+		btn_testButton.setOnClickListener(modeChange); // test
 	}
 	
 	public OnClickListener modeChange = new OnClickListener() {
@@ -47,6 +49,12 @@ public class TeacherActivity extends Activity {
 				Intent intent_vote = new Intent();
 				intent_vote.setClass(TeacherActivity.this, TVoteActivity.class);
 				startActivity(intent_vote); 
+				TeacherActivity.this.finish(); 
+				break;
+			case R.id.btn_Test: // test
+				Intent intent_test = new Intent();
+				intent_test.setClass(TeacherActivity.this, NewVoteActivity.class);
+				startActivity(intent_test); 
 				TeacherActivity.this.finish(); 
 				break;
 
