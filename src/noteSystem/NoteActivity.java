dcde6eb8d871339da826f37e67ee09f1603deaf3
Jia ching Jian <sagiters1128@gmail.com>
2015-04-12 22:37:53 +0800

    改進listview效率

--- src/noteSystem/NoteActivity.java
+++ src/noteSystem/NoteActivity.java
@@ -15,98 +15,153 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.AbsListView;
+import android.widget.AbsListView.OnScrollListener;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.ListView;
+
 /**
  * 
  * @author JiaChing
  *
  */
 public class NoteActivity extends Activity {
+	boolean shouldRefresh = true, isRefreshing = false;
+	boolean shouldLoadData = true, isLoadingData = false;
+
 	private ListView listViewNote;
+	private View viewFooter;
+	private View viewHead;
 	private ImageButton buttonCreateNote;
 	private ImageButton buttonBack;
+
 	private NoteListAdapter noteListAdapter;
+	private Button buttonCreateReply;
 	private String user = "jiachingTest";
+
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_note);
-		
-		
+
+//		viewHead = LayoutInflater.from(this).inflate(R.layout.note_head,
+//				null);
+//		viewFooter = LayoutInflater.from(this).inflate(
+//				R.layout.note_footer, null);
+
 		listViewNote = (ListView) findViewById(R.id.note_listView);
+//		listViewNote.addHeaderView(viewHead, null, false);
+//		listViewNote.addFooterView(viewFooter, null, false);
+
+		noteListAdapter = new NoteListAdapter(NoteActivity.this);
+
 		buttonBack = (ImageButton) findViewById(R.id.note_buttonGoBack);
 		buttonCreateNote = (ImageButton) findViewById(R.id.note_buttonCreate);
 		buttonCreateNote.setOnClickListener(createNoteListener);
-		noteListAdapter = new NoteListAdapter(NoteActivity.this);
 		listViewNote.setAdapter(noteListAdapter);
-//		ParseObject note = new ParseObject("NoteSystem");
-//        note.put("author","jiaching");
-//        note.put("message", "success1");
-//        note.put("rabbit_count",1);
-//        note.put("reply_count", 0);
-//        note.put("is_Hidden", false);
-//        note.put("is_Delete", false);
-//        note.put("is_Solved", false);
-//        note.saveInBackground();
 	}
+
 	/**
 	 * 
-	 * @param username username
-	 * @param message user's message
+	 * @param username
+	 *            username
+	 * @param message
+	 *            user's message
 	 */
-	public void insertNote(String username,String message) {
-	  ParseObject note = new ParseObject("NoteSystem");
-      note.put("author",username);
-      note.put("message", message);
-      note.put("rabbit_count",1);
-      note.put("reply_count", 0);
-      note.put("is_Hidden", false);
-      note.put("is_Delete", false);
-      note.put("is_Solved", false);
-      note.saveInBackground();
+	public void insertNote(String username, String message) {
+		ParseObject note = new ParseObject("NoteSystem");
+		note.put("author", username);
+		note.put("message", message);
+		note.put("rabbit_count", 1);
+		note.put("reply_count", 0);
+		note.put("is_Hidden", false);
+		note.put("is_Delete", false);
+		note.put("is_Solved", false);
+		note.saveInBackground();
 	}
-	
-	
-	
+//
+//	private ListView.OnScrollListener listViewScrollListener = new OnScrollListener() {
+//
+//		@Override
+//		public void onScrollStateChanged(AbsListView view, int scrollState) {
+//			if (scrollState == OnScrollListener.SCROLL_STATE_IDLE) {
+//		         //當ListView拉到頂或底時
+//		        if (shouldRefresh) {//當ListView拉到頂
+//		                if (!isRefreshing) noteListAdapter.updateNoteDate();//沒在更新資料時
+//		               
+//		                listViewNote.setSelection(1);//不管更不更新，都移到第一項
+//		        }
+//		        if (shouldLoadData && !isLoadingData) { 
+//		        	noteListAdapter.updateNoteDate(); 
+//		        	}
+//		        //當ListView拉到底,且沒在載入資料時
+//		    }
+//
+//		}
+//
+//		@Override
+//		public void onScroll(AbsListView view, int firstVisibleItem,
+//				int visibleItemCount, int totalItemCount) {
+//			// TODO Auto-generated method stub
+//			shouldLoadData = false;
+//			shouldRefresh = false;
+//			if (firstVisibleItem == 0) {// 拉到頂時
+//				shouldRefresh = true;
+//			} else if (firstVisibleItem + visibleItemCount == totalItemCount) {
+//				// 拉到底時
+//				if (listViewNote.getCount() < 60) {
+//					shouldLoadData = true;
+//				} else {// 只是測試用，如果超過60筆資料就不要再載入了
+//					viewFooter.setVisibility(View.GONE);
+//					listViewNote.removeFooterView(viewFooter);
+//					shouldRefresh = false;
+//				}
+//			}
+//		}
+//	};
+
+
+
 	private Button.OnClickListener createNoteListener = new OnClickListener() {
 
 		@Override
 		public void onClick(View v) {
 			// TODO Auto-generated method stub
-			View customDialog = getLayoutInflater().inflate(R.layout.note_create_note_layout, null);
-			final EditText editTextNote =(EditText) customDialog.findViewById(R.id.note_cteate_editText_message);
+			View customDialog = getLayoutInflater().inflate(
+					R.layout.note_create_note_layout, null);
+			final EditText editTextNote = (EditText) customDialog
+					.findViewById(R.id.note_cteate_editText_message);
 			new AlertDialog.Builder(NoteActivity.this)
-		    .setView(customDialog)
-		    .setNegativeButton("cancel", new DialogInterface.OnClickListener() {
+					.setView(customDialog)
+					.setNegativeButton("cancel",
+							new DialogInterface.OnClickListener() {
 
-				@Override
-				public void onClick(DialogInterface dialog, int which) {
-					// TODO Auto-generated method stub
-					
-				}
-		    	
-		    })
-		    .setPositiveButton("Submit", new DialogInterface.OnClickListener() {
+								@Override
+								public void onClick(DialogInterface dialog,
+										int which) {
+									// TODO Auto-generated method stub
 
-				@Override
-				public void onClick(DialogInterface dialog, int which) {
-					// TODO Auto-generated method stub
-					insertNote(user, editTextNote.getText().toString());
-					noteListAdapter.updateNoteDate();
-					noteListAdapter.notifyDataSetChanged();
-				
-				}
-		    	
-		    })
-		    .create()
-		    .show();
+								}
+
+							})
+					.setPositiveButton("Submit",
+							new DialogInterface.OnClickListener() {
+
+								@Override
+								public void onClick(DialogInterface dialog,
+										int which) {
+									// TODO Auto-generated method stub
+									insertNote(user, editTextNote.getText()
+											.toString());
+									noteListAdapter.updateNoteDate();
+									noteListAdapter.notifyDataSetChanged();
+
+								}
+
+							}).create().show();
 		}
 	};
 
-	
-	
-	
 }
--- src/noteSystem/NoteListAdapter.java
+++ src/noteSystem/NoteListAdapter.java
@@ -13,27 +13,32 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class NoteListAdapter extends BaseAdapter {
 	private Context activityContext;
 	private LayoutInflater inflater;
 	private List<ParseObject> note_list;
+	private NoteTag noteTag;
 	ParseQuery<ParseObject> query;
+
 	public NoteListAdapter(Context context) {
 		// TODO Auto-generated constructor stub
+
 		activityContext = context;
 		inflater = LayoutInflater.from(context);
 		query = ParseQuery.getQuery("NoteSystem");
 		try {
 			note_list = query.find();
-			Log.v("test",String.valueOf(note_list.size()));
+			Log.v("test", String.valueOf(note_list.size()));
 		} catch (ParseException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 	}
+
 	public void updateNoteDate() {
 		try {
 			note_list = query.find();
@@ -64,11 +69,36 @@ public class NoteListAdapter extends BaseAdapter {
 	@Override
 	public View getView(int position, View convertView, ViewGroup parent) {
 		// TODO Auto-generated method stub
-		convertView = inflater.inflate(R.layout.note_row, null);
-		TextView tv = (TextView) convertView.findViewById(R.id.note_tv_user_message);
-		TextView tv2 = (TextView) convertView.findViewById(R.id.note_tv_username);
-		tv.setText(note_list.get(position).getString("message"));
-		tv2.setText(note_list.get(position).getString("author"));
+
+		if (convertView == null) {
+			// 取得listItem容器 view
+			convertView = inflater.inflate(R.layout.note_row, null);
+
+			// 建構listItem內容view
+			noteTag = new NoteTag(
+					(TextView) convertView.findViewById(R.id.note_tv_username),
+					(TextView) convertView
+							.findViewById(R.id.note_tv_user_message),
+					(TextView) convertView
+							.findViewById(R.id.tv_note_rabbit_count),
+					(Button) convertView
+							.findViewById(R.id.btn_note_create_reply));
+
+			// 設置容器內容
+			convertView.setTag(noteTag);
+
+		} else {
+			noteTag = (NoteTag) convertView.getTag();
+		}
+
+		Note note = new Note(activityContext, noteTag.buttonDisaplyReply,
+				position);
+
+		noteTag.tvUserName.setText(note_list.get(position).getString("author"));
+		noteTag.tvUserMessage.setText(note_list.get(position).getString(
+				"message"));
+		noteTag.tvRabbitCount.setText(String.valueOf(note_list.get(position)
+				.getInt("rabbit_count")));
 		return convertView;
 	}
 
