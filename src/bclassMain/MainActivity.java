3baa5cb068eec2500dd12ccad8a6270ace1707d5
how <harry4512@hotmail.com.tw>
2015-04-12 16:14:38 +0800

    UI design

--- src/bclassMain/MainActivity.java
+++ src/bclassMain/MainActivity.java
@@ -24,7 +24,7 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		
+		getActionBar().hide();
 		init();
 	}
 
